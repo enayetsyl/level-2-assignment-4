f6f5659785426685e830cfb405aba2472a2195ae
enayetsyl <enayetflweb@gmail.com>
2024-08-26 18:51:52 +0600

    store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,12 +1,10 @@
 // store.ts
 import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
-import counterReducer from './features/counterSlice';
 
 const store = configureStore({
   reducer: {
-    count: counterReducer,
-    // [apiSlice.reducerPath]: apiSlice.reducer,
+   
   },
   // middleware: (getDefaultMiddleware) =>
   //   getDefaultMiddleware().concat(apiSlice.middleware),
